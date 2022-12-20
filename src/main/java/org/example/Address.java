f28cdd86d54ad693ffae6e518c8808134c63da46
Valentina <GruendlingerM190208@sus.htl-grieskirchen.at>
2022-12-20 18:44:25 +0100

    final finished

--- src/main/java/org/example/Address.java
+++ src/main/java/org/example/Address.java
@@ -3,6 +3,6 @@ package org.example;
 public record Address(String street, int houseNumber, int zipCode, String country, String land) {
     @Override
     public String toString() {
-        return zipCode + " " + country + " " + street + " " + houseNumber + " in " + land;
+        return String.format("| %-10s | %-10d | %-10d | %-10s | %-10s |", street, houseNumber, zipCode, country, land);
     }
 }
--- src/main/java/org/example/Job.java
+++ src/main/java/org/example/Job.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import java.text.NumberFormat;
+
 public record Job(String jobtitle, String department, double salary, String workStartTime, String workEndTime) {
+    @Override
+    public String toString() {
+        NumberFormat nf = NumberFormat.getInstance();
+        nf.setMaximumFractionDigits(2);
+        return  String.format("| %-15s | %-15s | %-5s€ | %-5s | %-5s |", jobtitle, department, nf.format(salary), workStartTime, workEndTime);
+    }
 }
